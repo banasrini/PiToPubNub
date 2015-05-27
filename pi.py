68e6e766c196345124345bc6e8818d323c1d7aaf
Bhavana Srinivas <bana.srini@gmail.com>
2015-05-27 12:30:45 -0700

    Update pi.py

--- pi.py
+++ pi.py
@@ -6,8 +6,8 @@ pi = iotbridge(publish_key = 'demo', subscribe_key = 'demo', uuid = 'PI')
 channel = 'iotchannel'
 message = "hello from pi"
  
-def callbackfn(message):
-  print(message)
+def callbackfn(m, n):
+  print(m)
  
 pi.send(channel, message)
 pi.connect(channel, callbackfn)
